d2471fc0624bbca413819f5cf76b65064d4cd74c
unknown <timothyseah.t.e@gmail.com>
2020-03-18 18:26:07 +0800

    Add SimpleRamp model

---
+++ Assets/Scripts/Marble.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marble : MonoBehaviour
+{
+            // Returns closest domino to position pos
+    public static Ramp FindClosestRamp(Vector3 pos) {
+        Ramp result = null;
+        float shortestDist = float.PositiveInfinity;
+        var e = Ramp.Pool.GetEnumerator();
+        while(e.MoveNext()) {
+            float currDist = (e.Current.transform.position - pos).sqrMagnitude;
+            if(currDist < shortestDist) {
+                result = e.Current;
+                shortestDist = currDist;
+            }
+        }
+        return result;
+    }
+}
---
+++ Assets/Scripts/Ramp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ramp : MonoBehaviour
+{
+    public readonly static HashSet<Ramp> Pool = new HashSet<Ramp>();
+
+    private void OnEnable() {
+        Ramp.Pool.Add(this);
+    }
+ 
+    private void OnDisable() {
+        Ramp.Pool.Remove(this);
+    }
+
+
+}
