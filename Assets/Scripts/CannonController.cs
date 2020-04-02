213311a40b7f463d4f7cd4a1e88ed420a622bd42
unknown <timothyseah.t.e@gmail.com>
2020-04-02 17:19:30 +0800

    Add Cannon and other prefabs

---
+++ Assets/Scripts/CannonController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonController : MonoBehaviour
+{   
+
+    public GameObject cannonBall;
+    Rigidbody cannonballRB;
+    private Transform shotPos;
+    public GameObject explosion;
+    private float firePower;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {   
+        if (other.tag == "Marble") {
+
+            FireCannon();
+        }
+    }
+
+    public void FireCannon() {
+        shotPos = this.transform.Find("shotPos").transform;
+        // shotPos.rotation = transform.rotation;
+        firePower = 15;
+        GameObject cannonBallCopy = Instantiate(cannonBall, shotPos.position, shotPos.rotation) as GameObject;
+        cannonballRB = cannonBallCopy.GetComponent<Rigidbody>();
+        cannonballRB.AddForce(shotPos.forward * firePower);
+        Instantiate(explosion, shotPos.position, shotPos.rotation);
+
+    }
+}
